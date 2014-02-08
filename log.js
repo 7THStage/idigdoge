28303dea23619ed9825c5dd99ac73f641abc24d4
7THStage <idigdoge@gmail.com>
2014-02-08 03:40:45 -0800

    Logging update

--- log.js
+++ log.js
@@ -3,9 +3,18 @@ function makeMessage(args) {
 	for (var i = 0, a; i < args.length; i += 1) {
 		a = args[i];
 		
-		if (typeof a === 'string') msgs.push(a);
+		if (a === null) msgs.push('null');
+		else if (a === undefined) msgs.push('undefined');
+		else if (typeof a === 'boolean') msgs.push(a ? 'true' : 'false');
+		else if (typeof a === 'string') msgs.push(a);
 		else if (typeof a === 'number') msgs.push(a.toString());
-		else if (typeof a === 'object' && 'toString' in a) msgs.push(a.toString());
+		else if (typeof a === 'object') {
+			try {
+				if ('toString' in a) {
+					msgs.push(JSON.stringify(a));
+				}
+			} catch (e) {}
+		}
 	}
 	
 	return msgs.join(' ');
