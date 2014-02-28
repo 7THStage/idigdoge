f75c8d6615329ebb644844f06a6fd43737badaec
SEan <hi.na3s@gmail.com>
2014-02-27 17:27:43 -0800

    Redis module?

--- redis.js
+++ redis.js
@@ -4,15 +4,15 @@ var log = require('./log');
 var config = require('./config');
 
 // var client = redis.createClient(config.redis || '');
-if (process.env.REDISTOGO_URL) {
-	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var client = redis.createClient(rtg.port, rtg.hostname);
-	client.auth(rtg.auth.split(":")[1]);
-	log.info('Using Addon REDISTOGO on Heroku');
+// if (process.env.REDISTOGO_URL) {
+// 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+// 	var client = redis.createClient(rtg.port, rtg.hostname);
+// 	client.auth(rtg.auth.split(":")[1]);
+// 	log.info('Using Addon REDISTOGO on Heroku');
 
-} else {
+// } else {
     var client = redis.createClient(config.redis || '');
-}
+// }
 
 // For serving up fast error pages if it's down
 client.ready = false;
