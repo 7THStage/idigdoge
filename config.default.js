e1458aea258807ae99782e595a84fe5fefd6a95e
SEan <hi.na3s@gmail.com>
2014-02-27 16:50:21 -0800

    Changed config.js to config.default.js

--- config.default.js
+++ config.default.js
@@ -15,42 +15,35 @@ module.exports = {
 	, rejectAssumedStales: true
 	// Configuration for outgoing emails
 	, email: {
-		address: 'sean@ideafablabs.com'
+		address: 'hello@example.com'
 		// For information on transport and settings, see the documentation for nodemailer at https://github.com/andris9/Nodemailer#possible-transport-methods
 		, transport: 'Sendmail'
 		, settings: {}
 	}
 	// The default port for the API server; it can also be set using the NODE_PORT environment variable
 	, server: {
-		port: 9877
+		port: 9876
 	}
 	// It works on a predictive model, so configure these values appropriately for how much you have been making
 	, payouts: {
-		targetPerHour: 0.4
-		, maxPerShare: 0.0004
+		targetPerHour: 25
+		, maxPerShare: 0.005
 		, minWithdraw: 1
 	}
 	// The pool you're connecting to; set active to false when developing so you don't constantly disconnect and reconnect
 	, stratum: {
-		host: 'mint.hashstrike.com'
-		, port: 3336
-		, username: 'harvest.mint'
-		, password: 'fhd87932nfd8*(84'
+		host: ''
+		, port: 3333
+		, username: ''
+		, password: ''
 		, active: true
 	}
-	// , stratum: {
-	// 	host: 'stratum.fast-pool.com'
-	// 	, port: 3002
-	// 	, username: 'sandwich.doge1'
-	// 	, password: 'meowrawr27'
-	// 	, active: true
-	// }
 	// This is used for payouts in the processing/payout.js file; do not put your pool information here
-	// , rpc: {
-	// 	host: ''
-	// 	, port: 22555
-	// 	, username: ''
-	// 	, password: ''
-	// 	, accountName: ''
-	// }
+	, rpc: {
+		host: ''
+		, port: 22555
+		, username: ''
+		, password: ''
+		, accountName: ''
+	}
 };
