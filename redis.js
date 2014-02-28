4204e56d1e3ad276eb3513057ebb7e6559a6b298
SEan <hi.na3s@gmail.com>
2014-02-27 17:03:55 -0800

    Added condition to check for heroku environment
    Ugly for now.

--- redis.js
+++ redis.js
@@ -3,7 +3,14 @@ var redis = require('redis');
 var log = require('./log');
 var config = require('./config');
 
-var client = redis.createClient(config.redis || '');
+//Compatibility with Heroku
+if (process.env.REDISTOGO_URL) {
+	log.info("Redis:"+rtg.auth.split(":")[0]+":"+rtg.auth.split(":")[1]);
+	client.auth(rtg.auth.split(":")[1]);
+	log.info('Using Addon REDISTOGO');
+} else {
+    var client = redis.createClient(config.redis || '');
+}
 
 // For serving up fast error pages if it's down
 client.ready = false;
